7ef25e6582172cf33e819d2863531311e6d97717
Mineleyend <cvesgaduran@gmail.com>
2021-11-08 19:00:19 -0500

    Main Menu BtnInteractions

--- app/src/main/java/com/example/favoritapp/MainActivity.java
+++ app/src/main/java/com/example/favoritapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.favoritapp;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,5 +37,30 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });*/
+
+        Button btnMapa = findViewById(R.id.main_btnMapa);
+        Button btnFavoritos = findViewById(R.id.main_btnFavoritos);
+        ImageButton btnAcercaDe = findViewById(R.id.main_btnAcercaDe);
+
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Mapa.class));
+            }
+        });
+
+        btnFavoritos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Favoritos.class));
+            }
+        });
+
+        btnAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AcercaDe.class));
+            }
+        });
     }
 }
