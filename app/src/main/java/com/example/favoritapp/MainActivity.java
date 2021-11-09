d59f7794673fe6ffb1f0d3e8c7ee54748cf00fe7
Mineleyend <cvesgaduran@gmail.com>
2021-11-08 19:46:24 -0500

    Main Menu

--- app/src/main/java/com/example/favoritapp/MainActivity.java
+++ app/src/main/java/com/example/favoritapp/MainActivity.java
@@ -17,27 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        Button btnAcceder = (Button) findViewById(R.id.main_btnAcceder);
-        EditText txtEmail = (EditText) findViewById(R.id.main_edtEmail);
-        EditText txtClave = (EditText) findViewById(R.id.main_edClave);
-
-        btnAcceder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = txtEmail.getText().toString();
-                String clave = txtClave.getText().toString();
-
-                if(email.isEmpty() || clave.isEmpty()){
-                    AlertDialog.Builder mensaje = new AlertDialog.Builder(view.getContext());
-                    mensaje.setTitle(R.string.main_btnAcceder_errorTitulo);
-                    mensaje.setMessage(R.string.main_btnAcceder_errorMensaje);
-                    mensaje.create();
-                    mensaje.show();
-                }
-            }
-        });*/
-
         Button btnMapa = findViewById(R.id.main_btnMapa);
         Button btnFavoritos = findViewById(R.id.main_btnFavoritos);
         ImageButton btnAcercaDe = findViewById(R.id.main_btnAcercaDe);
