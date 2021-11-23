9cdffbd262a45160a3f6f07e4bd352afee0e9e98
JoseElver <joseelverar@gmail.com>
2021-11-23 14:54:51 -0500

    fragmentos

--- app/src/main/java/com/example/favoritapp/FragmentDetallesSitio.java
+++ app/src/main/java/com/example/favoritapp/FragmentDetallesSitio.java
@@ -3,62 +3,60 @@ package com.example.favoritapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentDetallesSitio#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.favoritapp.modelos.Sitios;
+import com.example.favoritapp.viewmodels.SitiosViewModel;
+
+
 public class FragmentDetallesSitio extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public FragmentDetallesSitio() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentDetallesSitio.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentDetallesSitio newInstance(String param1, String param2) {
         FragmentDetallesSitio fragment = new FragmentDetallesSitio();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalles_sitio, container, false);
+
+        View vista = inflater.inflate(R.layout.fragment_detalles_sitio, container, false);
+
+        EditText txtTipo = (EditText) vista.findViewById(R.id.fragmento_detalles_txtTipo);
+        EditText txtNombre = (EditText) vista.findViewById(R.id.fragmento_detalles_txtNombre);
+        EditText txtDescripcion = (EditText) vista.findViewById(R.id.fragmento_detalles_txtDescripcion);
+        EditText txtLatitud = (EditText) vista.findViewById(R.id.fragmento_detalles_txtLatitud);
+        EditText txtLongitud = (EditText) vista.findViewById(R.id.fragmento_detalles_txtLongitud);
+
+        SitiosViewModel usuariovm = ViewModelProviders.of(getActivity()).get(SitiosViewModel.class);
+        usuariovm.getSitio().observe(getViewLifecycleOwner(), new Observer<Sitios>() {
+            @Override
+            public void onChanged(Sitios sit) {
+                txtTipo.setText(sit.getTipo());
+                txtNombre.setText(sit.getNombre());
+                txtDescripcion.setText(sit.getDescripcion());
+                txtLatitud.setText(String.valueOf(sit.getLatitud()));
+                txtLongitud.setText(String.valueOf(sit.getLongitud()));
+            }
+        });
+
+        return vista;
     }
 }
